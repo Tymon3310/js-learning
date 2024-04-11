6d36e7e8c42b9f77cd722bfe0d3564c9fa7c5adf
Tymon3310 <82083570+Tymon3310@users.noreply.github.com>
2024-04-11 13:07:39 +0200

    Update

--- script.ts
+++ script.ts
@@ -1,8 +1,3 @@
-
-    const aa = document.getElementById('aa') as HTMLElement;
-    if (aa) {
-        aa.innerHTML = "hello js";
-    }
     console.log("wczytano");
     let x = 8;
     let y = 5;
@@ -223,17 +218,43 @@ console.log(text6.padStart(10, "j")); //przedłuża początek
 
 console.log(text6.repeat(5)); //powtórz
 
-console.log(text4.replace("Caco DecoMoreno", "Harry Potter")) //zamień
-console.log(text4.replaceAll("a ", " Harry Potter ")) //zamień wszystkie
+console.log(text4.replace("Caco DecoMoreno", "Harry Potter")); //zamień
+console.log(text4.replaceAll("a ", " Harry Potter ")); //zamień wszystkie
 
-console.log(text5.trim().split(" ")[2]) //podziel na tablice i wybierz element
+console.log(text5.trim().split(" ")[2]); //podziel na tablice i wybierz element
 
-console.log(text4.indexOf("to")) //szukanie
-console.log(text4.lastIndexOf("jest"))
-console.log(text4.indexOf("jest", 20)) //szukanie od 20 znaku
+console.log(text4.indexOf("to")); //szukanie
+console.log(text4.lastIndexOf("jest"));
+console.log(text4.indexOf("jest", 20)); //szukanie od 20 znaku
 
-console.log(text4.includes("Potter")) //true jeśli jest
+console.log(text4.includes("Potter")); //true jeśli jest
 
-console.log(text4.startsWith("C")) //true jeśli zaczyna sie od C
-console.log(text4.endsWith("e")) //true jeśli kończy sie na e (tutaj będzie false)
+console.log(text4.startsWith("C")); //true jeśli zaczyna sie od C
+console.log(text4.endsWith("e")); //true jeśli kończy sie na e (tutaj będzie false)
 
+let text7 = "krowę";
+
+console.log(`Kupię ${text6 + "ka"} i ${text7}`); //Template string - string w stringu 
+let r = 3;
+console.log(`Pole koła o promieniu ${r} to ${(r * Math.PI).toFixed(2)}`);
+
+let kodHTML = `<h2>Test</h2> 
+<ol>
+    <li>Raz</li> 
+    <li>Dwa</li>
+    <li>Trzy</li>
+</ol>`;
+document.getElementById("Template").innerHTML = kodHTML; //template HTML
+
+let dec = 100;
+console.log(dec + " do binarnego to: " + dec.toString(2)); //do binarnego
+console.log(dec + " do ósemkowgo to: " + dec.toString(8)); //do ósemkowego
+console.log(dec + " do dwunastkowego to: " + dec.toString(12)); // do dwunastkowego
+console.log(dec + " do szestanstkowgo to: " + dec.toString(16)); //do szesnastkowego
+
+let hex = 0xF21;
+console.log(hex)
+console.log("0xF21 do dziesiętnego to: " + hex.toString(10)); //z szesnastkowego do dziesiętnego
+
+let bin = 0b01010101;
+console.log("01010101 do dziesiętnego to: " + bin.toString(10)); // z binarnego do dziesiętnego
